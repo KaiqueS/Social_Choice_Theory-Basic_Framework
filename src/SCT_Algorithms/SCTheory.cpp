2bbe2c707f7e469c7a7f24f2cf1df1d196d73866
Kaíque <KaiqueS@users.noreply.github.com>
2018-06-25 20:45:33 -0400

    Rank
    Added a ranking function. Checks how each pair of alternatives is ranked in each agent's ordering. Returns a general ranking of alternatives

--- src/SCT_Algorithms/SCTheory.cpp
+++ src/SCT_Algorithms/SCTheory.cpp
@@ -5,6 +5,12 @@
 #include "options.h"
 #include "pairsofopts.h"
 
+
+/* Possible optimizations: binary search in rank_generation. Harder, better, faster, stronger.
+ *						   order agent's orderings according to alternatives' values
+ *						   a ranking class/struct. Easier to manipulates, when compared to tuples
+*/
+
 // Generates, without repetition, all combinations of pairs of alternatives
 template<typename Prefs> std::vector<PairsOfOpts<Prefs>> pair_generation( std::vector<Agent<Prefs>>& listofagents ){
 
@@ -12,7 +18,7 @@ template<typename Prefs> std::vector<PairsOfOpts<Prefs>> pair_generation( std::v
 
 	std::vector<PairsOfOpts<Prefs>> paircomp{ };
 
-	// get all possible combination of pairs - ok
+	// get all possible combination of pairs, including repeated pairs
 	for( int i = 0; i < listofagents[ 0 ].get_preferences( ).size( ); ++i ){
 
 		compairs.xpref = listofagents[ 0 ].get_preferences( )[ i ];
@@ -40,41 +46,84 @@ template<typename Prefs> std::vector<PairsOfOpts<Prefs>> pair_generation( std::v
 
 		for( int j = i; j < paircomp.size( ); ++j ){
 
+			// checks if there are pairs a and b s.t. b == inverse( a ), i.e., a == ( x, y ) and b == ( y, x )
+			// adds to No Repeated Pairs only one version of the repeated pairs, i.e., ( x, y ) OR ( y, x ),
+			// but never both
 			if( paircomp[ j ].xpref == std::get<0>( pairs ) && paircomp[ j ].ypref == std::get<1>( pairs ) &&
-			    paircomp[ j ].xpref != std::get<1>( pairs ) && paircomp[ j ].ypref != std::get<0>( pairs )){
+				paircomp[ j ].xpref != std::get<1>( pairs ) && paircomp[ j ].ypref != std::get<0>( pairs )){
 
 				noreppairs.push_back( paircomp[ i ] );
 			}
 		}
 	}
 
-	// return ordering
+	// returns all combinations without repetitions
 	return noreppairs;
 }
 
-/*  */
-template<typename Prefs> Options<Prefs> condorcet_paradox( std::vector<Agent<Prefs>>& listofagents ){
+// Ranks alternatives. The ranking has a form of a vector of quintuples ( x, y, xval, yval, ival ), where
+// x and y are the alternatives, and the vals represent how many agents prefer one over the other. ival
+// represents indifference. The ranking operates in accord to how agents ranks pairs of alternatives
+template<typename Prefs> std::vector<std::tuple<Options<Prefs>, Options<Prefs>, int, int, int>> rank_generation( std::vector<Agent<Prefs>>& listofagents ){
 
-	Options<Prefs> result{ };
-
+	// Holder for a pair of options
 	PairsOfOpts<Prefs> compairs{ };
 
 	std::vector<PairsOfOpts<Prefs>> ordering = pair_generation( listofagents );
 
-	// check each combination for all agents
+	// quintuples ( x, y, xval, yval, ival ) and then map occurrences into val
+	std::tuple<Options<Prefs>, Options<Prefs>, int, int, int> quintuples{ };
+
+	std::vector<std::tuple<Options<Prefs>, Options<Prefs>, int, int, int>> ranking{ };
+
+	// Checks how a pair ( x, y ) is ranked for each agent
 	for( int i = 0; i < ordering.size( ); ++i ){
 
+		// Hold both alternatives that will be checked
 		compairs.xpref = ordering[ i ].xpref;
 		compairs.ypref = ordering[ i ].ypref;
 
-		std::tuple<Options<Prefs>, Options<Prefs>, int, int> quadruples{ };
-
 		for( int j = 0; j < listofagents.size( ); ++j ){
 
-			// quadruples ( x, y, xval, yval ) and then map occurrences into val
-			if( listofagents[ i ].get_preferences( )[ j ] == )
+			for( int k = 0; k < listofagents[ j ].get_preferences( ).size( ); ++ k ){
+
+				// Search for x in agent's preferences
+				if( listofagents[ j ].get_preferences( )[ k ].get_alternatives( ) == compairs.xpref.get_alternatives( ) )
+
+					std::get<0>( quintuples ) = listofagents[ j ].get_preferences( )[ k ];
+
+				// Search for y in agent's preferences
+				if( listofagents[ j ].get_preferences( )[ k ].get_alternatives( ) == compairs.ypref.get_alternatives( ) )
+
+					std::get<1>( quintuples ) = listofagents[ j ].get_preferences( )[ k ];
+			}
+
+			// Compares x and y values. Increments xval, yval, or ival accordingly
+			if( std::get<0>( quintuples ).get_value( ) > std::get<1>( quintuples ).get_value( ) )
+
+				++std::get<2>( quintuples );
+
+			else if( std::get<0>( quintuples ).get_value( ) < std::get<1>( quintuples ).get_value( ) )
+
+				++std::get<3>( quintuples );
+
+			else if( std::get<0>( quintuples ).get_value( ) == std::get<1>( quintuples ).get_value( ) )
+
+				++std::get<4>( quintuples );
 		}
+
+		// Stores the ranked tuple
+		ranking.push_back( quintuples );
+
+		// Resets the tuple for a new ranking
+		quintuples = { };
 	}
 
-	// one counter per pair
+	// returns a vector of quintuples
+	return ranking;
+}
+
+template<typename Prefs> void condorcet_paradox( std::vector<Agent<Prefs>>& listofagents ){
+
+	std::vector<std::tuple<Options<Prefs>, Options<Prefs>, int, int, int>> rank = rank_generation( listofagents );
 }
