fcfa1435e572ef0483a924bd73657f56c008601b
Kaíque P. Santos <KaiqueS@users.noreply.github.com>
2019-05-17 18:32:37 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,34 +11,32 @@
 // procedure, i.e., allow for the user to input a threshold that, when a given option surpasses
 // the latter, it becomes the winner or one of the winners. Add that to GUI
 // TODO: let the user decide between majority or list based procedures - GUI
-// TODO: make it possible for one to enable/disable indifference -> Create a vector with values from 0 to profile.size.
-// Select a value from the vector, assign it to a option, remove from the vector, granted that there are no repeated
-// values in the vector. This will guarantee no-indifference
+// TODO: make it possible for one to enable/disable indifference -> FOCUS ON THIS!
 // TODO: make a container of alternatives. Let the agents push back the alternatives and assign a random value to them,
 // this will make things more similar to how SCT works
-// TODO: consider dealing with rvalues of Profiles in each functor, instead of lvalues - EXCEPT sctheory.hpp/.cpp
+// TODO: increasing_merge_sort and decreasing_merge_sort
 
-void test1( ){
+void test1() {
 
 	Preferencematrix newmtx{ };
 
-	newmtx.set_matrix( 5, 5 );
+	newmtx.set_matrix(5, 5);
 
-	Population listofagents( newmtx.size( ) );
+	Population listofagents(newmtx.size());
 
 	std::cout << newmtx;
 
-	listofagents.initialize_population( newmtx );
+	listofagents.initialize_population(newmtx);
 
 	std::cout << "\n";
 
-	std::cout << listofagents[ 3 ] << "\n";
+	std::cout << listofagents[3] << "\n";
 
 	std::cout << "\n";
 
 	Rank rank{ };
 
-	rank.generate_ranking( listofagents );
+	rank.generate_ranking(listofagents);
 
 	std::cout << rank << "\n";
 
@@ -48,9 +46,9 @@ void test1( ){
 
 	Graph graph{ };
 
-	graph.initialize_graph( newmtx );
+	graph.initialize_graph(newmtx);
 
-	graph.make_graph( newmtx, rank );
+	graph.make_graph(newmtx, rank);
 
 	//std::thread graphthread( [ &graph, &newmtx, &rank ]( ){ graph.make_graph( newmtx, rank ); } );
 
@@ -60,7 +58,7 @@ void test1( ){
 
 	//std::cout << graph[ 0 ][ SocialPrefNode::worsethan_index{ 0 } ] -> get_id( ) << '\n';
 
-	print_graph( graph );
+	print_graph(graph);
 
 	//qualified_majority_rule( graph );
 
@@ -74,11 +72,11 @@ void test1( ){
 	 //std::cin >> procedure
 	 //pass procedure as argument to arrow function
 
-	SCT::Arrow_Impossibility arrow( newmtx, rank, listofagents, graph );
+	SCT::Arrow_Impossibility arrow(newmtx, rank, listofagents, graph);
 
 	SCT::Qualified_majority_rule maj;
 
-	maj( listofagents );
+	maj(listofagents);
 
 	SCT::Simple_majority_rule majority;
 
@@ -86,18 +84,16 @@ void test1( ){
 
 	SCT::Borda_count borda;
 
-	Profile count = borda( listofagents );
+	Profile count = borda(listofagents);
 
 	//Profile res = proport( listofagents );
 
 	SCT::Borda_count bordinha;
 
-	bordinha( listofagents );
+	bordinha(listofagents);
 
 	/*for( std::vector<int>::size_type i = 0; i < res.size( ); ++i ){
-
 		if( res[ i ].get_status( ) == true )
-
 			std::cout << res[ i ] << " ";
 	}*/
 
@@ -116,7 +112,7 @@ void test1( ){
 	//		std::cout << "Oh, damn!.\n\n";
 }
 
-void test2( ){
+void test2() {
 
 	Graph graph{ };
 
@@ -124,17 +120,17 @@ void test2( ){
 
 	Rank rank{ };
 
-	mtx.set_matrix( 45, 45 );
+	mtx.set_matrix(45, 45);
 
-	Population population( mtx.get_matrix( ).size( ) );
+	Population population(mtx.get_matrix().size());
 
-	population.initialize_population( mtx );
+	population.initialize_population(mtx);
 
-	rank.generate_ranking( population );
+	rank.generate_ranking(population);
 
-	graph.initialize_graph( mtx );
+	graph.initialize_graph(mtx);
 
-	graph.make_graph( mtx, rank );
+	graph.make_graph(mtx, rank);
 
 	SCT::Qualified_majority_rule maj{ };
 
@@ -142,32 +138,32 @@ void test2( ){
 
 	SCT::Simple_majority_rule simple{ };
 
-	Profile social = simple( population );
+	Profile social = simple(population);
 
 	//sct::Procedure newproc = { sct::Qualified_majority_rule( ) };
 
-	Profile result = maj( rank );
+	Profile result = maj(rank);
 
 	Graph newgraph{ };
 
-	Profile newres = count( population );
+	Profile newres = count(population);
 
-	std::cout << "The majority winner is: " << result[ 0 ] << "\n\n";
+	std::cout << "The majority winner is: " << result[0] << "\n\n";
 
-	std::cout << "The borda winner is: " << std::max_element( newres.begin( ), newres.end( ) )->get_opt( ) << "\n\n";
+	std::cout << "The borda winner is: " << std::max_element(newres.begin(), newres.end())->get_opt() << "\n\n";
 
-	std::cout << "The simple majority winner is: " << std::max_element( social.begin( ), social.end( ) )->get_opt( ) << "\n\n";
+	std::cout << "The simple majority winner is: " << std::max_element(social.begin(), social.end())->get_opt() << "\n\n";
 
-	for( std::vector<int>::size_type i = 0; i < result.size( ); ++i ){
+	for (std::vector<int>::size_type i = 0; i < result.size(); ++i) {
 
-		std::cout << result[ i ] << " ";
+		std::cout << result[i] << " ";
 	}
 
 	std::cout << "\n\n";
 
-	for( std::vector<int>::size_type i = 0; i < newres.size( ); ++i ){
+	for (std::vector<int>::size_type i = 0; i < newres.size(); ++i) {
 
-		std::cout << newres[ i ] << " ";
+		std::cout << newres[i] << " ";
 	}
 
 	//std::string res = sct::qualified_majority( graph );
@@ -179,38 +175,38 @@ void test2( ){
 	std::cout << "\n\n";
 }
 
-void test3( ){
+void test3() {
 
 	Preferencematrix mtx{ };
 
-	mtx.set_matrix( 4, 4 );
+	mtx.set_matrix(4, 4);
 
 	Rank rank{ };
 
-	rank.generate_ranking( mtx );
+	rank.generate_ranking(mtx);
 
 	std::cout << rank;
 
 	Population pop{ };
 
 	// Problem here
-	pop.initialize_population( mtx );
+	pop.initialize_population(mtx);
 
 	Profile prof{ };
 
-	prof = mtx[ 2 ];
+	prof = mtx[2];
 
-	Options opt = prof[ 1 ];
+	Options opt = prof[1];
 
-	find_opt( prof, opt );
+	find_opt(prof, opt);
 
-	prof.sort_by_opt( );
+	prof.sort_by_opt();
 
 	Graph graph{ };
 
-	graph.initialize_graph( pop );
+	graph.initialize_graph(pop);
 
-	graph.make_graph( mtx, rank );
+	graph.make_graph(mtx, rank);
 
 	SCT::Borda_count bordinha;
 
@@ -218,24 +214,24 @@ void test3( ){
 
 	SCT::Qualified_majority_rule newmaj;
 
-	SCT::Arrow_Impossibility arrow( mtx, rank, pop, graph );
+	SCT::Arrow_Impossibility arrow(mtx, rank, pop, graph);
 
 	//std::cout << arrow( newmaj ) << "\n";
 }
 
-void test4( ){
+void test4() {
 
-	Profile testprof = { Options( "d", false, 3 ), Options( "b", false, 1 ), Options( "c", false, 2 ), Options( "a", false, 0 ) };
-	Profile testprof2 = { Options( "e", false, 4 ), Options( "d", false, 3 ), Options( "b", false, 1 ), Options( "c", false, 2 ), Options( "a", false, 0 ) };
+	Profile testprof = { Options("d", false, 3), Options("b", false, 1), Options("c", false, 2), Options("a", false, 0) };
+	Profile testprof2 = { Options("e", false, 4), Options("d", false, 3), Options("b", false, 1), Options("c", false, 2), Options("a", false, 0) };
 
 	//merge_sort( testprof, 0, testprof.size( ) - 1 );
 	//merge_sort( testprof2, 0, testprof2.size( ) - 1 );
-	testprof.value_merge_sort( 0, testprof.size( ) - 1 );
-	testprof2.value_merge_sort( 0, testprof.size( ) - 1 );
+	testprof.value_merge_sort(0, testprof.size() - 1);
+	testprof2.value_merge_sort(0, testprof.size() - 1);
 
-	Profile profile = { Options( "d", false, 3 ), Options( "b", false, 1 ), Options( "c", false, 2 ), Options( "a", false, 0 ) };
+	Profile profile = { Options("d", false, 3), Options("b", false, 1), Options("c", false, 2), Options("a", false, 0) };
 
-	profile.value_merge_sort( 0, profile.size( ) - 1 );
+	profile.value_merge_sort(0, profile.size() - 1);
 
 	std::cout << testprof << "\n";
 
@@ -244,17 +240,17 @@ void test4( ){
 	std::cout << profile << "\n";
 }
 
-void test5( ){
+void test5() {
 
-	Profile profile = { Options( "a", false, 3 ) };
+	Profile profile = { Options("a", false, 3) };
 
 	Agent one = { profile, "one" };
 
 	Agent two = one;
 
-	two.set_id( "two" );
+	two.set_id("two");
 
-	if( one == two )
+	if (one == two)
 
 		std::cout << "true\n";
 
@@ -267,11 +263,11 @@ void test5( ){
 	Options opt{ "opt", false, 5 };
 	Options opt2{ "opt2", false, 3 };
 
-	if( opt > opt2 )
+	if (opt > opt2)
 
 		std::cout << "opt is greater than opt2\n";
 
-	if( opt != opt2 )
+	if (opt != opt2)
 
 		std::cout << "opt is not opt2\n";
 
@@ -282,25 +278,25 @@ void test5( ){
 	std::cout << opt;
 }
 
-void test6( ){
+void test6() {
 
 	// Since I know that the second profile holds the same relations as the first, I could use it as a test case. Could test IIA's ranking_comparison to see if it generates the
 	// second profile from the first to check for correctness
 
-	Preferencematrix original = { Profile( { Options( "a", false, 5 ), Options( "b", false, 4 ), Options( "c", false, 3 ), Options( "d", false, 2 ), Options( "e", false, 1 ) } ),
-								  Profile( { Options( "b", false, 5 ), Options( "c", false, 4 ), Options( "d", false, 3 ), Options( "e", false, 2 ), Options( "a", false, 1 ) } ),
-								  Profile( { Options( "c", false, 5 ), Options( "d", false, 4 ), Options( "e", false, 3 ), Options( "a", false, 2 ), Options( "b", false, 1 ) } ),
-								  Profile( { Options( "d", false, 5 ), Options( "e", false, 4 ), Options( "a", false, 3 ), Options( "b", false, 2 ), Options( "c", false, 1 ) } ),
-								  Profile( { Options( "e", false, 5 ), Options( "a", false, 4 ), Options( "b", false, 3 ), Options( "c", false, 2 ), Options( "d", false, 1 ) } ) };
+	Preferencematrix original = { Profile({ Options("a", false, 5), Options("b", false, 4), Options("c", false, 3), Options("d", false, 2), Options("e", false, 1) }),
+								  Profile({ Options("b", false, 5), Options("c", false, 4), Options("d", false, 3), Options("e", false, 2), Options("a", false, 1) }),
+								  Profile({ Options("c", false, 5), Options("d", false, 4), Options("e", false, 3), Options("a", false, 2), Options("b", false, 1) }),
+								  Profile({ Options("d", false, 5), Options("e", false, 4), Options("a", false, 3), Options("b", false, 2), Options("c", false, 1) }),
+								  Profile({ Options("e", false, 5), Options("a", false, 4), Options("b", false, 3), Options("c", false, 2), Options("d", false, 1) }) };
 
-	Preferencematrix prime = { Profile( { Options( "c", false, 5 ), Options( "d", false, 4 ), Options( "e", false, 3 ), Options( "a", false, 2 ), Options( "b", false, 1 ) } ),
-								  Profile( { Options( "e", false, 5 ), Options( "d", false, 4 ), Options( "a", false, 3 ), Options( "b", false, 2 ), Options( "c", false, 1 ) } ),
-								  Profile( { Options( "c", false, 5 ), Options( "b", false, 4 ), Options( "a", false, 3 ), Options( "d", false, 2 ), Options( "e", false, 1 ) } ),
-								  Profile( { Options( "b", false, 5 ), Options( "d", false, 4 ), Options( "a", false, 3 ), Options( "c", false, 2 ), Options( "e", false, 1 ) } ),
-								  Profile( { Options( "e", false, 5 ), Options( "a", false, 4 ), Options( "b", false, 3 ), Options( "c", false, 2 ), Options( "d", false, 1 ) } ) };
+	Preferencematrix prime = { Profile({ Options("c", false, 5), Options("d", false, 4), Options("e", false, 3), Options("a", false, 2), Options("b", false, 1) }),
+								  Profile({ Options("e", false, 5), Options("d", false, 4), Options("a", false, 3), Options("b", false, 2), Options("c", false, 1) }),
+								  Profile({ Options("c", false, 5), Options("b", false, 4), Options("a", false, 3), Options("d", false, 2), Options("e", false, 1) }),
+								  Profile({ Options("b", false, 5), Options("d", false, 4), Options("a", false, 3), Options("c", false, 2), Options("e", false, 1) }),
+								  Profile({ Options("e", false, 5), Options("a", false, 4), Options("b", false, 3), Options("c", false, 2), Options("d", false, 1) }) };
 
-	Rank left( original );
-	Rank right( prime );
+	Rank left(original);
+	Rank right(prime);
 
 	//left.order_ranking( );
 	//right.order_ranking( );
@@ -309,14 +305,14 @@ void test6( ){
 	SCT::Borda_count borda;
 	SCT::Qualified_majority_rule quali;
 
-	Profile first{ borda( original ) };
-	Profile second{ borda( prime ) };
+	Profile first{ borda(original) };
+	Profile second{ borda(prime) };
 
-	Profile third{ maj( original ) };
-	Profile four{ maj( prime ) };
+	Profile third{ maj(original) };
+	Profile four{ maj(prime) };
 
-	Profile five{ quali( original ) };
-	Profile six{ quali( prime ) };
+	Profile five{ quali(original) };
+	Profile six{ quali(prime) };
 
 	std::cout << original << "\n\n" << prime << "\n";
 
@@ -327,7 +323,7 @@ void test6( ){
 
 	std::cout << first << "\n" << second << "\n" << third << "\n" << four << "\n" << five << "\n" << six << "\n";
 
-	if( first == second )
+	if (first == second)
 
 		std::cout << "\ntrue\n\n";
 
@@ -336,21 +332,21 @@ void test6( ){
 		std::cout << "\nfalse\n\n";
 }
 
-void test7( ){
+void test7() {
 
 	bool val = false;
 
-	while( val == false ){
+	while (val == false) {
 
-		Preferencematrix matrix( 5, 5 );
+		Preferencematrix matrix(5, 5);
 
-		Rank rank( matrix );
+		Rank rank(matrix);
 
 		std::cout << matrix << "\n";
 
 		std::cout << rank << "\n";
 
-		if( rank.is_transitive( ) )
+		if (rank.is_transitive())
 
 			val = true;
 
@@ -358,32 +354,32 @@ void test7( ){
 	}
 }
 
-void test8( ){
+void test8() {
 
-	Preferencematrix original = { Profile( { Options( "a", false, 5 ), Options( "b", false, 4 ), Options( "c", false, 3 ), Options( "d", false, 2 ), Options( "e", false, 1 ) } ),
-								  Profile( { Options( "b", false, 5 ), Options( "c", false, 4 ), Options( "d", false, 3 ), Options( "e", false, 2 ), Options( "a", false, 1 ) } ),
-								  Profile( { Options( "c", false, 5 ), Options( "d", false, 4 ), Options( "e", false, 3 ), Options( "a", false, 2 ), Options( "b", false, 1 ) } ),
-								  Profile( { Options( "d", false, 5 ), Options( "e", false, 4 ), Options( "a", false, 3 ), Options( "b", false, 2 ), Options( "c", false, 1 ) } ),
-								  Profile( { Options( "e", false, 5 ), Options( "a", false, 4 ), Options( "b", false, 3 ), Options( "c", false, 2 ), Options( "d", false, 1 ) } ) };
+	Preferencematrix original = { Profile({ Options("a", false, 5), Options("b", false, 4), Options("c", false, 3), Options("d", false, 2), Options("e", false, 1) }),
+								  Profile({ Options("b", false, 5), Options("c", false, 4), Options("d", false, 3), Options("e", false, 2), Options("a", false, 1) }),
+								  Profile({ Options("c", false, 5), Options("d", false, 4), Options("e", false, 3), Options("a", false, 2), Options("b", false, 1) }),
+								  Profile({ Options("d", false, 5), Options("e", false, 4), Options("a", false, 3), Options("b", false, 2), Options("c", false, 1) }),
+								  Profile({ Options("e", false, 5), Options("a", false, 4), Options("b", false, 3), Options("c", false, 2), Options("d", false, 1) }) };
 
-    /*Preferencematrix prime = { Profile( { Options( "c", false, 5 ), Options( "d", false, 4 ), Options( "e", false, 3 ), Options( "a", false, 2 ), Options( "b", false, 1 ) } ),
-                                  Profile( { Options( "e", false, 5 ), Options( "d", false, 4 ), Options( "a", false, 3 ), Options( "b", false, 2 ), Options( "c", false, 1 ) } ),
-                                  Profile( { Options( "c", false, 5 ), Options( "b", false, 4 ), Options( "a", false, 3 ), Options( "d", false, 2 ), Options( "e", false, 1 ) } ),
-                                  Profile( { Options( "b", false, 5 ), Options( "d", false, 4 ), Options( "a", false, 3 ), Options( "c", false, 2 ), Options( "e", false, 1 ) } ),
-                                  Profile( { Options( "e", false, 5 ), Options( "a", false, 4 ), Options( "b", false, 3 ), Options( "c", false, 2 ), Options( "d", false, 1 ) } ) };*/
+	/*Preferencematrix prime = { Profile( { Options( "c", false, 5 ), Options( "d", false, 4 ), Options( "e", false, 3 ), Options( "a", false, 2 ), Options( "b", false, 1 ) } ),
+								  Profile( { Options( "e", false, 5 ), Options( "d", false, 4 ), Options( "a", false, 3 ), Options( "b", false, 2 ), Options( "c", false, 1 ) } ),
+								  Profile( { Options( "c", false, 5 ), Options( "b", false, 4 ), Options( "a", false, 3 ), Options( "d", false, 2 ), Options( "e", false, 1 ) } ),
+								  Profile( { Options( "b", false, 5 ), Options( "d", false, 4 ), Options( "a", false, 3 ), Options( "c", false, 2 ), Options( "e", false, 1 ) } ),
+								  Profile( { Options( "e", false, 5 ), Options( "a", false, 4 ), Options( "b", false, 3 ), Options( "c", false, 2 ), Options( "d", false, 1 ) } ) };*/
 
-	Rank one( original );
+	Rank one(original);
 
-    Preferencematrix prime{ };
+	Preferencematrix prime{ };
 
-    //Preferencematrix prime = generate_prime_profile( original, one );
-    Rank two( prime );
+	//Preferencematrix prime = generate_prime_profile( original, one );
+	Rank two(prime);
 
-    std::cout << prime << "\n";
+	std::cout << prime << "\n";
 
-    std::cout << one << "\n" << two << "\n";
+	std::cout << one << "\n" << two << "\n";
 
-    if( SCT::Borda_count( ).operator( )( original ) != SCT::Borda_count( ).operator( )( two ) )
+	if (SCT::Borda_count().operator( )(original) != SCT::Borda_count().operator( )(two))
 
 		std::cout << "true\n";
 
@@ -392,34 +388,40 @@ void test8( ){
 		std::cout << "false\n";
 }
 
-void test9( ){
+void test9() {
 
 	bool test = true;
 
-	while( test ){
+	while (test) {
 
-	Preferencematrix original( 4, 4 );
-	Rank rank( original );
+		Preferencematrix original(4, 4);
+		Rank rank(original);
 
-	Preferencematrix prime{ };
+		Preferencematrix prime{ };
 
-	//Preferencematrix prime = generate_prime_profile( original, rank );
-	Rank primerank( prime );
+		//Preferencematrix prime = generate_prime_profile( original, rank );
+		Rank primerank(prime);
 
-	std::cout << original << "\n" << prime << "\n";
+		std::cout << original << "\n" << prime << "\n";
 
-	std::cout << rank << "\n" << primerank << "\n";
+		std::cout << rank << "\n" << primerank << "\n";
 
-	if( rank_relations( rank, primerank ) == false )
+		if (rank_relations(rank, primerank) == false)
 
-		test = false;
+			test = false;
 	}
 }
 
-int main( ){
+void test10( ){
 
 	Preferencematrix matrix( 4, 4 );
 
 	Preferencematrix prime = generate_prime_profile( matrix );
+
+	std::cout << matrix << "\n" << prime << "\n";
 }
 
+int main() {
+
+	test10( );
+}
