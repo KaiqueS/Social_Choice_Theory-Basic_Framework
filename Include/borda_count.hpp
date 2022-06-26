f345343874b81368250583c6505dc382e6b9e768
KaiqueS <kaiquepsantos@protonmail.com>
2022-06-26 01:08:08 -0300

    ADD: started working on Borda Count procedure

---
+++ Include/borda_count.hpp
@@ -0,0 +1,32 @@
+#ifndef BORDA_COUNT_HPP
+#define BORDA_COUNT_HPP
+
+#pragma once
+
+#include "/Trabalho/Projetos/Cpp/Social_Choice/Include/procedure.hpp"
+
+namespace SCT{
+
+
+class Borda_Count : public Procedure{
+
+	public:
+
+		Borda_Count( ){ }
+		~Borda_Count( ){ }
+
+		virtual Profile operator( )( Preferencematrix& matrix );
+
+		virtual Options operator( )( Options& left, Options& right, Preferencematrix& matrix );
+
+		virtual void score( Preferencematrix& matrix ) override;
+
+	private:
+	
+		Profile winnerset{ };
+	};
+
+}
+
+
+#endif // BORDA_COUNT
